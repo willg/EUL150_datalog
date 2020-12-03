a86e0e7970d8a13b624ddceb293602e2f9b3883a
will <20484529+willg@users.noreply.github.com>
2020-12-03 21:24:36 +0900

    testing out load commands

--- control.py
+++ control.py
@@ -1,5 +1,35 @@
+import serial
+
+def init_gpib():
+    print("configuring GPIB controller")
+    print("")
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+    ser.write(b'++ver\n')
+    data = ser.read(100)
+    print(data)
+    ser.write(b'++auto 2\n')
+
+    ser.write(b'++auto \n')
+    data = ser.read(100)
+    print("auto: " + str(data))
+
+    return ser
+
 def main():
-    print("Hello")
+    ser = init_gpib()
+    ser.write(b"LOAD:?\n")
+    data = ser.read(100)
+    print(data)
+
+    ser.write(b"MEAS:V?\n")
+    data = ser.read(100)
+    print("voltage: " + str(data))
+
+    ser.write(b"MEAS:C?\n")
+    data = ser.read(100)
+    print("current: " + str(data))
+
+    ser.close()
 
 if __name__ == "__main__":
     main()
